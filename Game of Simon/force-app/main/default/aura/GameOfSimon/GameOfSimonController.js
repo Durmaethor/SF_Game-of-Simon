a0c63bc2d1878fc77d8881d2bcad3054b3b9b398
Ryan Kramer <rkramer613@gmail.com>
2021-07-09 11:27:12 -0600

    push at beginning of logic

--- Game of Simon/force-app/main/default/aura/GameOfSimon/GameOfSimonController.js
+++ Game of Simon/force-app/main/default/aura/GameOfSimon/GameOfSimonController.js
@@ -1,8 +1,43 @@
 ({
-    toggleOn : function(component, event, helper) {
+    onInit : function(component, event, helper) {
 
+    let order = [];
+    let playerOrder = [];
+    let flash;
+    let turn;
+    let good;
+    let compTurn;
+    let intervalId;
+    let strict = false;
+    let noise = true;
+    let on = false;
+    let win;
 
+    const turnCounter = document.querySelector("#turn");
+    const topLeft = document.querySelector("#topleft");
+    const topRight = document.querySelector("#topright");
+    const bottomLeft = document.querySelector("#bottomleft");
+    const bottomRight = document.querySelector("#bottomright");
+    const strictButton = document.querySelector("#strict");
+    const onButton = document.getElementById('on');
+    const startButton = document.querySelector("#start");
 
+    
 
-    }
+    },
+
+    onButton : function(component) {
+
+        if (onButton.checked == true) {
+            on = true;
+            turnCounter.innerHTML = "-";
+          } else {
+            on = false;
+            turnCounter.innerHTML = "";
+            clearColor();
+            clearInterval(intervalId);
+          }
+
+    },
+
 })
